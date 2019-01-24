2c3a1ac879f3c64be0bac3b46ca6d9847d610630
hkamath3 <hkamath3@gatech.edu>
2019-01-24 12:15:12 -0500

    Added Frontend Skeleton to Work on

---
+++ Vocapture/View Controllers/ModuleVC.swift
@@ -0,0 +1,30 @@
+//
+//  ModuleVC.swift
+//  Vocapture
+//
+//  Created by Harish Kamath on 1/24/19.
+//  Copyright © 2019 Harish Kamath. All rights reserved.
+//
+
+import UIKit
+
+class ModuleVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
