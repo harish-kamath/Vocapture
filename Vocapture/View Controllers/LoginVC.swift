0653083d9ce9c2f912875ab13628e5f9d4e5f544
ARathie <ashwin.rathie@gmail.com>
2019-02-05 18:05:20 -0500

    login aesthetics

--- Vocapture/View Controllers/LoginVC.swift
+++ Vocapture/View Controllers/LoginVC.swift
@@ -10,47 +10,52 @@ import UIKit
 import AVFoundation
 
 class LoginVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+    }
 
     var avPlayer: AVPlayer!
     var avPlayerLayer: AVPlayerLayer!
     var paused: Bool = false
-    
-    override func viewDidLoad() {
-        
-        let theURL = Bundle.main.url(forResource: "yolo_v3_1080p", withExtension: "mp4")
-        
-        avPlayer = AVPlayer(url: theURL!)
-        avPlayerLayer = AVPlayerLayer(player: avPlayer)
-        avPlayerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        avPlayer.volume = 0
-        avPlayer.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
-        
-        avPlayerLayer.frame = view.layer.bounds
-        view.backgroundColor = UIColor.clear;
-        view.layer.insertSublayer(avPlayerLayer, at: 0)
-        
-        NotificationCenter.default.addObserver(self,
-                                               selector: Selector("playerItemDidReachEnd:"),
-                                                         name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
-                                                         object: avPlayer.currentItem)
-    }
-    
-    func playerItemDidReachEnd(notification: NSNotification) {
-        let p: AVPlayerItem = notification.object as! AVPlayerItem
-        p.seek(to: CMTime.zero)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        avPlayer.play()
-        paused = false
-    }
-    
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        avPlayer.pause()
-        paused = true
-    }
+
+//    override func viewDidLoad() {
+//
+//        let theURL = Bundle.main.url(forResource: "yolo_v3_1080p", withExtension: "mp4")
+//
+//        avPlayer = AVPlayer(url: theURL!)
+//        avPlayerLayer = AVPlayerLayer(player: avPlayer)
+//        avPlayerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+//        avPlayer.volume = 0
+//        avPlayer.actionAtItemEnd = AVPlayer.ActionAtItemEnd.none
+//
+//        avPlayerLayer.frame = view.layer.bounds
+//        view.backgroundColor = UIColor.clear;
+//        view.layer.insertSublayer(avPlayerLayer, at: 0)
+//
+//        NotificationCenter.default.addObserver(self,
+//                                               selector: Selector("playerItemDidReachEnd:"),
+//                                                         name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+//                                                         object: avPlayer.currentItem)
+//    }
+//
+//    func playerItemDidReachEnd(notification: NSNotification) {
+//        let p: AVPlayerItem = notification.object as! AVPlayerItem
+//        p.seek(to: CMTime.zero)
+//    }
+//
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//        avPlayer.play()
+//        paused = false
+//    }
+//
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(animated)
+//        avPlayer.pause()
+//        paused = true
+//    }
 }
 
 
