f25370724cac41a4fe5cd8bbc1d9be84b5fce58f
hkamath3 <hkamath3@gatech.edu>
2019-02-04 19:51:47 -0500

    Info Logic to Camera View

--- Vocapture/View Controllers/CameraVC.swift
+++ Vocapture/View Controllers/CameraVC.swift
@@ -10,12 +10,35 @@ import UIKit
 
 class CameraVC: UIViewController {
 
+    @IBOutlet weak var LearningButton: UIButton!
+    @IBOutlet weak var ModulesButton: UIButton!
+    @IBOutlet weak var SettingsButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func expandMenu(_ sender:UIButton){
+        if(LearningButton.alpha < 1){
+            UIView.animate(withDuration: 0.5, animations: {
+            self.LearningButton.alpha = 1
+            self.ModulesButton.alpha = 1
+            self.SettingsButton.alpha = 1
+            }, completion: nil)
+        }
+        else{
+            UIView.animate(withDuration: 0.5, animations: {
+                self.LearningButton.alpha = 0
+                self.ModulesButton.alpha = 0
+                self.SettingsButton.alpha = 0
+            }, completion: nil)
+            
+        }
+    }
 
     /*
     // MARK: - Navigation
