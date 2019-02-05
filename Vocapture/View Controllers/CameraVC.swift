d7ce7f0c662885f6a120bc3702121f945663092c
ARathie <ashwin.rathie@gmail.com>
2019-02-04 20:20:45 -0500

    fixing things

--- Vocapture/View Controllers/CameraVC.swift
+++ Vocapture/View Controllers/CameraVC.swift
@@ -7,41 +7,19 @@
 //
 
 import UIKit
-import ARKit
 
-class CameraVC: UIViewController, ARSCNViewDelegate {
+class CameraVC: UIViewController {
 
     @IBOutlet weak var LearningButton: UIButton!
     @IBOutlet weak var ModulesButton: UIButton!
     @IBOutlet weak var SettingsButton: UIButton!
     @IBOutlet weak var InfoButton: UIButton!
     
-    @IBOutlet var sceneView: ARSCNView!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        sceneView.delegate = self
 
         // Do any additional setup after loading the view.
-    }
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-        
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
     }
     
     
@@ -82,23 +60,6 @@ class CameraVC: UIViewController, ARSCNViewDelegate {
             }, completion: nil)
             
         }
-    }
-    
-    
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 
     /*
