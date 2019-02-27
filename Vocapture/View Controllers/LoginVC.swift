bed2da5683411b312a76cc9d70b2a4720756fc40
ARathie <ashwin.rathie@gmail.com>
2019-02-26 23:05:19 -0500

    end of 2/26 beatifying UI then hiding all of it :(

--- Vocapture/View Controllers/LoginVC.swift
+++ Vocapture/View Controllers/LoginVC.swift
@@ -18,15 +18,35 @@ class LoginVC: UIViewController {
     @IBOutlet var skipLoginButton: UIButton!
     @IBOutlet var signUpButton: UIButton!
     @IBOutlet var dontHaveAccountLabel: UILabel!
+    @IBOutlet var logo: UIImageView!
+//    @IBOutlet var loginSelector: UISegmentedControl!
+    @IBOutlet var usernameInput: UITextField!
+    @IBOutlet var passwordInput: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
-        setupVieww()
+        setupVideo()
+        setupSelector()
+        
+        loginButton.layer.cornerRadius = loginButton.frame.size.width / 6
+        skipLoginButton.layer.cornerRadius = skipLoginButton.frame.size.width / 6
+        signUpButton.layer.cornerRadius = signUpButton.frame.size.width / 6
+        
+        // not rounding corners as intended, likely bc it's not a button
+        dontHaveAccountLabel.layer.cornerRadius = dontHaveAccountLabel.frame.size.width / 6
+        
+        loginButton.isHidden = true
+        skipLoginButton.isHidden = true
+        signUpButton.isHidden = true
+        dontHaveAccountLabel.isHidden = true
+        usernameInput.isHidden = true
+        passwordInput.isHidden = true
+        
     }
     
-    func setupVieww() {
+    func setupVideo() {
         let path = URL(fileURLWithPath: Bundle.main.path(forResource: "yolo_v3_1080p", ofType: "mov")!)
         let player = AVPlayer(url: path)
         let newLayer = AVPlayerLayer(player: player)
@@ -41,14 +61,38 @@ class LoginVC: UIViewController {
         
         NotificationCenter.default.addObserver(self, selector:  #selector(LoginVC.videoDidPlayToEnd(_:)), name: NSNotification.Name(rawValue: "AVPlayerItemDidPlayToEndTimeNotification"), object: player.currentItem)
         
-        loginButton.layer.cornerRadius = loginButton.frame.size.width / 6
-        skipLoginButton.layer.cornerRadius = skipLoginButton.frame.size.width / 6
-        signUpButton.layer.cornerRadius = signUpButton.frame.size.width / 6
         
-        // not rounding corners as intended, likely bc it's not a button
-        dontHaveAccountLabel.layer.cornerRadius = dontHaveAccountLabel.frame.size.width / 6
+    }
+    
+    func setupSelector() {
+        let signInOption = NSLocalizedString("Sign In", comment: "Sign into an existing account")
+        let signUpOption = NSLocalizedString("Sign Up", comment: "Create a new Account")
         
+        let loginSelector = UISegmentedControl(items: [signInOption, signUpOption])
         
+        loginSelector.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        loginSelector.selectedSegmentIndex = 0
+       
+        
+//        loginSelector.addTarget(self,
+//                                   action: #selector(MapViewController.mapTypeChanged(_:)), for: .valueChanged)
+        
+        loginSelector.translatesAutoresizingMaskIntoConstraints = false
+        
+        mainView.addSubview(loginSelector)
+        
+        let topConstraint = loginSelector.topAnchor.constraint(equalTo: logo.safeAreaLayoutGuide.bottomAnchor, constant: 8)
+
+        let margins = logo.layoutMarginsGuide
+
+        let leadingConstraint = loginSelector.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = loginSelector.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        loginSelector.isHidden = true
     }
     
     @objc func videoDidPlayToEnd(_ notification: Notification) {
