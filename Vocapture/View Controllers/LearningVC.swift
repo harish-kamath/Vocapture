e0b366f9916cf03c47772078e964ffb80966fdf2
Mayank <mayankkishore.in@gmail.com>
2019-02-26 23:08:06 -0500

    learning view controller and modules view controller updates

--- Vocapture/View Controllers/LearningVC.swift
+++ Vocapture/View Controllers/LearningVC.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class LearningVC: UIViewController {
+class LearningVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView:UITableView!
     @IBOutlet var wordsLearned: UIButton!
     @IBOutlet var modules: UIButton!
     
@@ -16,9 +18,33 @@ class LearningVC: UIViewController {
         super.viewDidLoad()
         wordsLearned.layer.cornerRadius = 4
         modules.layer.cornerRadius = 4
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : ModulesCell = tableView.dequeueReusableCell(withIdentifier: "modulescell", for: indexPath) as! ModulesCell
+        cell.labelText.text = "Module 1"
+        cell.progressBar.progress = 1
+        cell.progressBar.barBorderColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
+        cell.progressBar.barFillColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
+        cell.progressBar.barBackgroundColor = UIColor(red:0.77, green:0.93, blue:0.78, alpha:1.0)
+        cell.progressBar.barBorderWidth = 1
+        cell.progressBar.barFillInset = 2
+        cell.progressBar.labelTextColor = UIColor(red:0.35, green:0.80, blue:0.36, alpha:1.0)
+        cell.progressBar.progressLabelInsets = UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+        cell.progressBar.font = UIFont.boldSystemFont(ofSize: 18)
+        cell.progressBar.labelPosition = GTProgressBarLabelPosition.right
+        cell.progressBar.displayLabel = false
+        cell.progressBar.barMaxHeight = 12
+        cell.progressBar.direction = GTProgressBarDirection.anticlockwise
+        return cell
+    }
 
     /*
     // MARK: - Navigation
--- Vocapture/View Controllers/ModuleVC.swift
+++ Vocapture/View Controllers/ModuleVC.swift
@@ -23,7 +23,7 @@ class ModuleVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
